e9355492c9d44fe44657fe3ebf3c756e97e2dccf
BojanBlagojevic <bojanblagojevic06@gmail.com>
2020-09-10 13:13:01 +0200

    adding mobx

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Menu, Container, MenuItem, Button } from "semantic-ui-react";
+import ActivityStore from "../../app/stores/activityStore";
+import { observer } from "mobx-react-lite";
 
-interface IProps{
-  openCreateForm: () => void;
-}
 
-const NavBar: React.FC<IProps> = ({openCreateForm}) => {
+
+
+const NavBar: React.FC = () => {
+  const activityStore = useContext(ActivityStore)
   return (
     <Menu inverted>
       <Container>
@@ -15,11 +17,11 @@ const NavBar: React.FC<IProps> = ({openCreateForm}) => {
           </MenuItem>
         <Menu.Item name="Activities" />
         <MenuItem>
-            <Button onClick={openCreateForm} positive content="Create Activity"/>
+            <Button onClick={activityStore.openCreateForm} positive content="Create Activity"/>
         </MenuItem>
       </Container>
     </Menu>
   );
 };
 
-export default NavBar;
+export default observer(NavBar);
