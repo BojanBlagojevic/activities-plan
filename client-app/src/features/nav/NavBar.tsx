f3068b98730253450685674fed17c6247f585731
BojanBlagojevic <bojanblagojevic06@gmail.com>
2020-09-10 17:50:36 +0200

    routing

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,24 +1,24 @@
-import React, { useContext } from "react";
-import { Menu, Container, MenuItem, Button } from "semantic-ui-react";
-import ActivityStore from "../../app/stores/activityStore";
-import { observer } from "mobx-react-lite";
-
-
-
+import React from 'react';
+import { Menu, Container, Button } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import { NavLink } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
-  const activityStore = useContext(ActivityStore)
   return (
-    <Menu inverted>
+    <Menu fixed='top' inverted>
       <Container>
-          <MenuItem header>
-              <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
-              Reactivities
-          </MenuItem>
-        <Menu.Item name="Activities" />
-        <MenuItem>
-            <Button onClick={activityStore.openCreateForm} positive content="Create Activity"/>
-        </MenuItem>
+        <Menu.Item header as={NavLink} exact to='/'>
+          <img src='/assets/logo.png' alt='logo' style={{ marginRight: 10 }} />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name='Activities' as={NavLink} to='/activities' />
+        <Menu.Item>
+          <Button
+            as={NavLink} to='/createActivity'
+            positive
+            content='Create Activity'
+          />
+        </Menu.Item>
       </Container>
     </Menu>
   );
